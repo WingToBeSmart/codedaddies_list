3599ae1af87e5f2f6a377d39fc687ac3b6c7f259
wing chan <wingchan@gmstoneft.com>
2020-03-23 18:16:11 +0800

    update view.py for searching

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,7 @@
+import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
-import requests
+from requests.compat import quote_plus
 # Create your views here.
 
 BASE_CRAIGSLIST_URL = 'https://hongkong.craigslist.org/search/?query={}'
@@ -12,6 +13,10 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    # print(data)
     stuff_for_frontend = {
         'search': search,
     }
