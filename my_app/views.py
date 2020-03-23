09f1633fdf77f1558f23d806baf8cddb1940670c
wing chan <wingchan@gmstoneft.com>
2020-03-23 17:59:17 +0800

    First commit

---
+++ my_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+# Create your views here.
+
+BASE_CRAIGSLIST_URL = 'https://hongkong.craigslist.org/search/?query={}'
+
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
