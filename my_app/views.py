22a0cc6bd9ae7aa81d3668bcd6560bd030d141d7
wing chan <wingchan@gmstoneft.com>
2020-03-24 12:46:57 +0800

    add search function in this project

--- my_app/views.py
+++ my_app/views.py
@@ -2,9 +2,11 @@ import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 from requests.compat import quote_plus
+from . import models
 # Create your views here.
 
 BASE_CRAIGSLIST_URL = 'https://hongkong.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 def home(request):
@@ -13,11 +15,42 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
     final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
     response = requests.get(final_url)
     data = response.text
-    # print(data)
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+    # post_title = post_listings[0].find(class_='result-title').text
+    # post_url = post_listings[0].find('a').get('href')
+    # post_price = post_listings[0].find(class_='result-price').text
+    # print(post_title)
+    # print(post_url)
+    # print(post_price)
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(
+                class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append(
+            (post_title, post_url, post_price, post_image_url))
+
+    # print(final_postings)
     stuff_for_frontend = {
         'search': search,
+        'final_postings': final_postings
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
